97fb8c481b382910d8f118bde76f9b4cdea19af6
Archit Kumar <archit@wpi.edu>
2022-09-18 12:05:47 -0500

    Add 'base.html' template
    See #1

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("register", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register_page():
     return render_template("register.html")
