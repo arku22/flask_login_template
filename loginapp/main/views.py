dbc3a38beb3f924a84624aba94a08d65574db463
Archit Kumar <archit@wpi.edu>
2022-09-30 12:09:39 -0500

    Build out webapp 'about' pg
    See #13

--- loginapp/main/views.py
+++ loginapp/main/views.py
@@ -5,3 +5,8 @@ from flask import render_template
 @main.route("/")
 def index():
     return render_template("main/webapp_home.html")
+
+
+@main.route("/about")
+def about():
+    return render_template("main/about.html")
